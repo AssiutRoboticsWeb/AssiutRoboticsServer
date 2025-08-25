68e39ee3d5159a8642207ab60ba58b5c5d447e7f
mazen magdy <69149378+Mazen-magdy@users.noreply.github.com>
2025-08-25 23:56:18 +0300

    Revert "Fix login CORS issue & remove eslint file"
    This reverts commit 8be7a33e935c89ddb6bc5dc38a4954d39c905230.

---
+++ .eslintrc.js
@@ -0,0 +1,73 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Error prevention
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': 'error',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-undef': 'error',
+
+    // Code quality
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error',
+
+    // Best practices
+    eqeqeq: 'error',
+    curly: ['error', 'all'],
+    'no-eval': 'error',
+
+    // Node.js specific
+    'no-path-concat': 'error',
+    'no-process-exit': 'error',
+
+    // Async/Await
+    'no-async-promise-executor': 'error',
+    'no-promise-executor-return': 'error',
+
+    // Security
+    'no-implied-eval': 'error',
+    'no-new-func': 'error',
+
+    // Code style
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'trailing-comma': ['error', 'es5'],
+
+    // Spacing
+    'space-before-function-paren': ['error', 'always'],
+    'space-before-blocks': 'error',
+    'keyword-spacing': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+
+    // Line breaks
+    // 'linebreak-style': ['error', 'unix'],
+    'eol-last': 'error',
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js', '**/*.spec.js'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
+};
