0062962baf64a59a25f68fab4cd18ec3313a25db
Anthony Gaius <knkmam05@gmail.com>
2026-07-28 18:59:05 +0300

    Fix syntax error in require statement that breaks the server

--- routes/visit.js
+++ routes/visit.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const Visits = require('../models/visits");
-const Member = require('../models/member");
+const Visits = require('../models/visits');
+const Member = require('../models/member');
 
 const asyncWrapper = require("../middleware/asyncWrapper");
 
